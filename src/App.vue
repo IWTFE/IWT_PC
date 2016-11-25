d35b1e16f9b348a3c209ebbf923e3523b0e22cde
dsq3033 <303306984@qq.com>
2016-11-25 17:24:47 +0800

    modified:   src/App.vue 	new file:   src/assets/author.png 	new file:   src/assets/login-bj.jpg 	new file:   src/assets/logo.jpg 	modified:   src/components/Login.vue

--- src/App.vue
+++ src/App.vue
@@ -21,11 +21,11 @@ export default {
 	.bounce-enter-active {
 		animation: bounce-in .5s;
 	}
-	
+
 	.bounce-leave-active {
 		animation: bounce-out .2s;
 	}
-	
+
 	@keyframes bounce-in {
 		0% {
 			transform: scale(0);
@@ -37,7 +37,7 @@ export default {
 			transform: scale(1);
 		}
 	}
-	
+
 	@keyframes bounce-out {
 		0% {
 			transform: scale(1);
@@ -49,31 +49,28 @@ export default {
 			transform: scale(0);
 		}
 	}
-	
+
 	body {
-		/*background-color: #324057;*/
+
 		margin: 0px;
 		padding: 0px;
-		/*background: url(assets/bg1.jpg) center !important;
-		background-size: cover;*/
-		background: #1F2D3D;
 		font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, SimSun, sans-serif;
 		font-weight: 400;
 		-webkit-font-smoothing: antialiased;
 	}
-	
+
 	#app {
 		position: absolute;
 		top: 0px;
 		bottom: 0px;
 		width: 100%;
 	}
-	
+
 	.el-submenu [class^=fa] {
 		vertical-align: baseline;
 		margin-right: 10px;
 	}
-	
+
 	.el-menu-item [class^=fa] {
 		vertical-align: baseline;
 		margin-right: 10px;
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,18 +1,34 @@
 <template>
-  <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px" class="demo-ruleForm card-box loginform">
-    <h3 class="title">系统登录</h3>
-    <el-form-item prop="account">
-      <el-input type="text" v-model="ruleForm2.account" auto-complete="off" placeholder="账号"></el-input>
-    </el-form-item>
-    <el-form-item prop="checkPass">
-      <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="密码"></el-input>
-    </el-form-item>
-    <el-checkbox v-model="checked" checked style="margin:0px 0px 35px 0px;">记住密码</el-checkbox>
-    <el-form-item style="width:100%;">
-      <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2">登录</el-button>
-      <!--<el-button @click.native.prevent="handleReset2">重置</el-button>-->
-    </el-form-item>
-  </el-form>
+
+<div>
+  <div class="logo container">
+    <img src="../assets/logo.jpg">
+  </div>
+  <div class="login container">
+    <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px" class="demo-ruleForm card-box loginform">
+      <h3 class="title">用户登录</h3>
+      <el-form-item prop="account">
+        <el-input type="text" v-model="ruleForm2.account" auto-complete="off" placeholder="账号"></el-input>
+      </el-form-item>
+      <el-form-item prop="checkPass">
+        <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="密码"></el-input>
+      </el-form-item>
+      <el-form-item prop="auth" style="line-height:0">
+        <el-col :span="12">
+          <el-input type="text" v-model="ruleForm2.auth" auto-complete="off" placeholder="验证码"></el-input>
+        </el-col>
+        <el-col :span="10" :offset="1">
+          <img src="../assets/author.png" style="height:36px;">
+        </el-col>
+      </el-form-item>
+      <el-form-item style="width:100%;">
+        <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2">登录</el-button>
+        <!--<el-button @click.native.prevent="handleReset2">重置</el-button>-->
+      </el-form-item>
+    </el-form>
+  </div>
+  <div class="bottom container">版权所有：Copyright © 2016 英泰伟业科技（北京）有限公司</div>
+</div>
 </template>
 
 <script>
@@ -57,26 +73,27 @@
 </script>
 
 <style scoped>
-  .card-box {
-    padding: 20px;
-    /*box-shadow: 0 0px 8px 0 rgba(0, 0, 0, 0.06), 0 1px 0px 0 rgba(0, 0, 0, 0.02);*/
+  .container { margin:0 auto; }
+  .logo { margin-top:70px; width:1024px; }
+  .login{ width:486px;height:416px;padding:42px 0 0 538px;background:url(../assets/login-bj.jpg) no-repeat; }
+  .bottom{ width:1024px;height:46px;line-height:46px;font-size:12px;color:#fff;text-align:center;background:#bc1b21;margin-top:14px;}
+  .login .card-box {
+    padding: 30px;
     -webkit-border-radius: 5px;
     border-radius: 5px;
     -moz-border-radius: 5px;
     background-clip: padding-box;
     margin-bottom: 20px;
-    background-color: #F9FAFC;
-    margin: 120px auto;
-    width: 400px;
-    border: 2px solid #8492A6;
+    background-color: #fff;
+    width: 270px;
   }
-  
+
   .title {
     margin: 0px auto 40px auto;
     text-align: center;
     color: #505458;
   }
-  
+
   .loginform {
     width: 350px;
     padding: 35px 35px 15px 35px;
